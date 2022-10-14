c6631e71ba40f6dacc7c8b8032ddc395bc9f4eee
Nikita Bert <n.bert@ya.ru>
2022-10-14 19:15:47 +0300

    IMPR: refactor dao -> repository

--- src/main/java/com/example/springboot/service/UserServiceImpl.java
+++ src/main/java/com/example/springboot/service/UserServiceImpl.java
@@ -1,44 +1,48 @@
 package com.example.springboot.service;
 
-import com.example.springboot.dao.UserDAO;
 import com.example.springboot.model.User;
+import com.example.springboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final UserDAO userDAO;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserServiceImpl(UserDAO userDAO) {
-        this.userDAO = userDAO;
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
     public List<User> getAllUsers() {
-        return userDAO.getAllUsers();
+        List<User> userList = new ArrayList<>();
+        userRepository.findAll().forEach(userList::add);
+        return userList;
     }
 
     @Override
     public User getUser(int id) {
-        return userDAO.getUser(id);
+        return userRepository.findById(id).orElse(null);
     }
 
     @Override
     public void saveUser(User user) {
-        userDAO.saveUser(user);
+        userRepository.save(user);
     }
 
     @Override
     public void updateUser(int id, User user) {
-        userDAO.updateUser(id, user);
+        user.setId(id);
+        userRepository.save(user);
     }
 
     @Override
     public void deleteUser(int id) {
-        userDAO.deleteUser(id);
+        userRepository.deleteById(id);
     }
 }
